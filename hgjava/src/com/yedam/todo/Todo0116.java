ee30854daa83baa8db9562e93a74be4aecb34e24
kimjingyeong <kimjingyeong@localhost>
2024-01-17 08:48:54 +0900

    숙

--- hgjava/src/com/yedam/todo/Todo0116.java
+++ hgjava/src/com/yedam/todo/Todo0116.java
@@ -10,10 +10,19 @@ public class Todo0116 {
 	System.out.println("값을 입력>>> ");
 	String input = scn.nextLine();
 	//input.equals("quit")
+	while() {
+	System.out.print("값을 입력>>> ('quit'을 입력하면 종료) ");
+	String input = scanner.nextLine();
 	
+	if(input equals.('quit')) {
+		System.out.println("<<end of prog>> ");
+	break;
+	}
+	
+	
+	}
 	// 입력값은 홍길동입니다.
-	
-	
+
 	// "quit" 값이 들어오면 <<end of prog>>
 	
 	}
